10e06357d29ade93195e7af2fcf56f6756e724a9
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-05 13:04:45 +0530

    test case for the problem 6

---
+++ testcase/test6.js
@@ -0,0 +1,6 @@
+let problem6 = require('../problem/problem6.js')
+let inventory = require('../problem/inventory.js')
+
+let result = problem6(inventory)
+const myJSON = JSON.stringify(result);
+console.log(myJSON)
