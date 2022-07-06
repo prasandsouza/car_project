ed08141af58a621f8fe7281ff6b56f0e8e60c344
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-06 12:59:05 +0530

    updating file foe test case 5 on comparing object inventory file is used not year file changes made on if statement and on push

--- problem/problem5.js
+++ problem/problem5.js
@@ -1,10 +1,10 @@
-module.exports = function problem5(inventory) {
+module.exports = function problem(inventory) {
     let caryear = []
     for (let index = 0; index < inventory.length; index++) 
     {
-        if(inventory[index].car_year<2000)
+        if(inventory[index]<2000)
         {
-            caryear.push(inventory[index].car_year)
+            caryear.push(inventory[index])
         }
         
     }
