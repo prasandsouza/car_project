3ef4f069038cab4ad3289ce834852ad4791d972b
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:13:45 +0530

    changed code logic when inventory is not there and changed if the id other than assigned

--- problem1.js
+++ problem1.js
@@ -1,15 +1,34 @@
 module.exports = function problem1(inventory, id)
 {
+    if(!inventory){
+        return [];
+    }
+    if(!id){
+        return id
+    }
+    if(inventory.length==0)
+    {
+        return inventory
+    }
+
+    if(Array.isArray(inventory))
+    {
     let car;
     for (let i = 0; i <= inventory.length; i++) 
     {
+        if(id>50){return id}
         if(inventory[i].id === id)
         {
             car = inventory[i];
             break;
         }
-        
     }
     let result = "car "+car.car_year+" "+car.car_make+" "+car.car_model
-    return result;
+    let result1 = [result];
+    return result1;
+    }
+    else{
+        return inventory
+    }
+    
 }
