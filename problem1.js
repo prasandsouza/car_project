582daf1709cdb98d3df082d43a35e99f3fb214ca
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 16:22:35 +0530

    added code to check id is valid or not

--- problem1.js
+++ problem1.js
@@ -1,34 +1,32 @@
 module.exports = function problem1(inventory, id)
 {
-    if(!inventory){
+    if(!inventory && !id)
+    {
         return [];
-    }
-    if(!id){
-        return id
     }
     if(inventory.length==0)
     {
         return inventory
     }
-
-    if(Array.isArray(inventory))
+    if(id==null)
     {
-    let car;
-    for (let i = 0; i <= inventory.length; i++) 
-    {
-        if(id>50){return id}
-        if(inventory[i].id === id)
-        {
-            car = inventory[i];
-            break;
-        }
-    }
-    let result = "car "+car.car_year+" "+car.car_make+" "+car.car_model
-    let result1 = [result];
-    return result1;
-    }
-    else{
-        return inventory
+        return id
+    }else{
+            if(Array.isArray(inventory))
+            {
+                let car;
+                for (let i = 0; i <= inventory.length; i++) 
+                {
+                    if(id>50){return id}
+                    if(inventory[i].id === id)
+                    {
+                        return inventory[i];
+                    }
+                }
+            }
+            else{
+                return inventory
+            }
     }
     
 }
