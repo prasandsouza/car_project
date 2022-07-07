8a4754648756864ff6dbbe4da6077a61b3e9e809
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:18:13 +0530

    changed the code for if testing id is not given

--- test/test1.js
+++ test/test1.js
@@ -1,15 +1,14 @@
 let inventory = require('../inventory');
 let prob1 = require('../problem1');
-
-const testId = 33;
-const result = prob1(inventory, testId);
-
-const finalResult = "car 2011 Jeep Wrangler";
-if(result === finalResult){ 
-    console.log(result);
+let testId = 33;
+if(testId==null){
+    console.log("enter id to search")
 }
-else 
+else
 {
-console.log("Results Didn't Match");
+    const result = prob1(inventory, testId);
+    if(result){
+    
+        console.log(result);
 }
-
+}
