37798016ce5aa0eef168748cd112d211f8d261bd
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:32:55 +0530

    added code for checking is there is other datatype or empty inventory data in it

--- problem3.js
+++ problem3.js
@@ -1,5 +1,15 @@
 module.exports = function(inventory)
-{
+{   
+    if(!inventory){
+        return [];
+    }
+    if(Array.isArray(inventory))
+    {
     let model = inventory.sort((a, b) => a.car_model.toLowerCase() > b.car_model.toLowerCase() ? 1 : -1);
     return model;
+    }
+    else
+    {
+        return inventory
+    }
 }
