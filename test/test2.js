957bf0a00dd25d8bdc7e3d1cf568157c082c1f62
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:23:19 +0530

    removed logic code from test file

--- test/test2.js
+++ test/test2.js
@@ -1,10 +1,7 @@
 let inventory = require('../inventory');
 let problem2 = require('../problem2');
 const result = problem2(inventory);
-const finalResult = "Last car is a Lincoln Town Car";
-if(result === finalResult){ 
-    console.log(result);
-}else{
-console.log("Results Didn't Match");
-}
+
+console.log(result);
+
 
