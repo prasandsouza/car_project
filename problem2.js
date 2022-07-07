dca0745ab3854b351ac5d8a5c625261a5e8bfa7b
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:27:38 +0530

    added code for checking is there is other datatype or empty inventory data in it

--- problem2.js
+++ problem2.js
@@ -1,3 +1,13 @@
 module.exports = function problem2(inventory){
+    if(!inventory){
+        return [];
+    }
+    if(Array.isArray(inventory)){
+
     return ("Last car is a " + inventory[inventory.length-1].car_make + " " +inventory[inventory.length-1].car_model)
+    }
+    else
+    {
+        return inventory
+    }
 }
