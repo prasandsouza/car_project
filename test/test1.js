e6ce5cea1de94aa82291dcb48db745a2dc9fe190
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-07 15:45:09 +0530

    involked logic part from testfile

--- test/test1.js
+++ test/test1.js
@@ -1,14 +1,7 @@
 let inventory = require('../inventory');
 let prob1 = require('../problem1');
-let testId = 33;
-if(testId==null){
-    console.log("enter id to search")
-}
-else
-{
-    const result = prob1(inventory, testId);
-    if(result){
-    
-        console.log(result);
-}
-}
+let testId 
+
+const result = prob1(inventory, testId);
+console.log(result);
+
