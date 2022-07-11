03a3ba78b9c65e49550692b0a02dd1b235d2efcd
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-11 16:35:46 +0530

    changed the code for empty inventory and empty testid

--- problem1.js
+++ problem1.js
@@ -1,32 +1,15 @@
-module.exports = function problem1(inventory, id)
-{
-    if(!inventory && !id)
-    {
+module.exports = function problem1(inventory, id) {
+    if (!inventory || !id) {
         return [];
     }
-    if(inventory.length==0 && inventory.length>20)
+    else 
     {
-        return []
-    }
-    if(id==null)
-    {
-        return []
-    }else{
-            if(Array.isArray(inventory))
-            {
-                let car;
-                for (let i = 0; i <= inventory.length; i++) 
-                {
-                    if(id>50){return id}
-                    if(inventory[i].id === id)
-                    {
-                        return inventory[i];
-                    }
-                }
+        for (let i = 0; i < inventory.length; i++) {
+            if (inventory[i].id === id) {
+                return inventory[i];
             }
-            else{
-                return []
-            }
+        }
+        
     }
 
 }
